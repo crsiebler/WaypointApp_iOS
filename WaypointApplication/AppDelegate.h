e076cd110e42d968875d893d333cf5f726a0d8e7
Cory R Siebler <cory.siebler@gmail.com>
2015-03-23 12:40:46 -0700

    Added base project

---
+++ WaypointApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WaypointApplication
+//
+//  Created by Cory Siebler on 3/23/15.
+//  Copyright (c) 2015 Cory Siebler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WaypointApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WaypointApplication
+//
+//  Created by Cory Siebler on 3/23/15.
+//  Copyright (c) 2015 Cory Siebler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
